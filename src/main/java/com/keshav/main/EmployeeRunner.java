4d8ee48cf479150f40f36acd302590aaf2ef7b56
Keshav Kumar <keshavkumar93761@gmail.com>
2026-01-15 15:35:57 +0530

    update

--- src/main/java/com/keshav/main/EmployeeRunner.java
+++ src/main/java/com/keshav/main/EmployeeRunner.java
@@ -7,7 +7,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-//import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Configuration;
 
 import com.keshav.entity.Employee;
  
@@ -17,14 +17,14 @@ public class EmployeeRunner{
 		
 		Employee emp = new Employee("Pintu","Male",40500);
 		
-//      using Configuration
+//      using "Configuration"
 //		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 //		SessionFactory sessionFactory = cfg.buildSessionFactory();
 		
 // 		Configuration single line(Method chaining)
 //		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 		
-//		using StandardServiceRegistry
+//		using "StandardServiceRegistry"
 //		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 //		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
 //		SessionFactory sessionFactory = metadata.buildSessionFactory();
@@ -37,5 +37,28 @@ public class EmployeeRunner{
 		
 		session.persist(emp);;
 		tx.commit();
+		
+//		get method(now deprecated)
+//		Employee employee = session.get(Employee.class,5);
+//		System.out.println(employee);
+//		
+//		Employee employee = session.get(Employee.class,10);   //it will give null as 10th Object is not found in database
+//		System.out.println(employee);
+		
+		//Now find is used instead of get
+//		Employee employee = session.find(Employee.class,10);   //it will give null
+//		System.out.println(employee);
+		
+//		Employee employee = session.find(Employee.class,3);
+//		System.out.println(employee);
+		
+//		load method
+//		Employee employee = new Employee();
+//		session.load(employee,4);
+//		System.out.println(employee);
+		
+//		session.load(employee,10);   //it will give ObjectNotFoundException as 10th Object is not found in database
+//		System.out.println(employee);
+
 	}
 }
